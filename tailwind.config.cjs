4fe3581a588985d5f22e518d7d54c40656571a62
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-10-23 15:35:35 +0800

    Let tailwind scan ts and tsx files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: ['class'],
-	content: ['./components/**/*.{js,jsx}', './app/**/*.{js,jsx}'],
+	content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
 	theme: {
 		container: {
 			center: true,

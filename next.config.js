8e7f2f153dff6a783f1cf94032ae6c9f72bad951
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-11-02 07:57:18 +0800

    Add .vercel to .gitignore and update dependencies.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const config = {
 	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+				port: '',
+			},
+		],
 		domains: ['loremflickr.com'],
 	},
 }

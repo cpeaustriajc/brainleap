b781d3a47dfa6c34140890411b62884328b7acdd
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-11-02 07:59:10 +0800

    Refactor authentication function to setup profile

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,16 +1,7 @@
 'use server'
-import { signIn } from '@/lib/auth'
 
-export async function authenticate(
-	_prevState: string | undefined,
-	formData: FormData,
-) {
-	try {
-		await signIn('credentials', Object.fromEntries(formData))
-	} catch (error) {
-		if ((error as Error).message.includes('CredentialsSignin')) {
-			return 'CredentialsSignin'
-		}
-		throw error
-	}
+
+
+export async function setupProfile(formData: FormData) {
+
 }

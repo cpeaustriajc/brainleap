e13e175197376dff28e934598c65c67d4dfb5117
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-11-02 07:58:31 +0800

    Add SessionProvider import to Providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
 
 type Props = {
 	children: React.ReactNode
 }
+
+export { SessionProvider }
 
 export function Providers({ children }: Props) {
 	return (

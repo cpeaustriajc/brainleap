4bcde15b4559be1e2f94846909fb1bbc4933e4bf
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-11-16 10:30:39 +0800

    Cleanup login-form, add callback url

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,22 +1,14 @@
 'use client'
 
-import { useFormState, useFormStatus } from 'react-dom'
 import { Button } from './ui/button'
-import { Label } from './ui/label'
-import { Input } from './ui/input'
-import { ArrowRightIcon } from '@radix-ui/react-icons'
 import { signIn } from 'next-auth/react'
 
 export function LoginForm() {
-	return <Button onClick={() => signIn('google')}>Sign In with Google</Button>
-}
-
-function LoginButton() {
-	const { pending } = useFormStatus()
-
 	return (
-		<Button aria-disabled={pending} className="w-full">
-			Log In <ArrowRightIcon className="ml-auto h-5 w-5" />
+		<Button
+			onClick={() => signIn('google', { callbackUrl: '/profile/setup' })}
+		>
+			Sign In with Google
 		</Button>
 	)
 }

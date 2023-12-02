0170bcdef4f68df76edd3af9434b9535f5f41dca
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-02 15:13:59 +0800

    Simplify AddClass form component

--- components/add-class.tsx
+++ components/add-class.tsx
@@ -1,70 +1,37 @@
 import { Button } from './ui/button'
-import { Database } from '@/lib/database.types'
 import { DialogContent, DialogHeader } from './ui/dialog'
 import { Input } from './ui/input'
-import { Form, FormControl, FormField, FormItem, FormLabel } from './ui/form'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { Textarea } from './ui/textarea'
-import { createClient } from '@/lib/supabase/client'
-
-const classFormSchema = z.object({
-	id: z.string().uuid().optional(),
-	title: z.string().min(3).max(20),
-	description: z.string().min(10).max(160),
-})
+import { Label } from './ui/label'
+import { addClass } from '@/lib/actions'
 
 export function AddClassDialog() {
-	const form = useForm<z.infer<typeof classFormSchema>>({
-		resolver: zodResolver(classFormSchema),
-		defaultValues: {
-			id: '',
-			description: '',
-			title: '',
-		},
-	})
-	async function addClass() {
-		const supabase = createClient()
-	}
 	return (
 		<>
 			<DialogContent>
 				<DialogHeader>Add Class</DialogHeader>
-				<Form {...form}>
-					<form className="space-y-8">
-						<FormField
+				<form className="space-y-8" action={addClass}>
+					<div>
+						<Label htmlFor="title">Class Title</Label>
+						<Input
+							type="text"
+							id="title"
 							name="title"
-							control={form.control}
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Class Title</FormLabel>
-									<FormControl>
-										<Input
-											placeholder="Class Title"
-											{...field}
-										/>
-									</FormControl>
-								</FormItem>
-							)}
+							placeholder="Class Title"
 						/>
-						<FormField
+					</div>
+					<div>
+						<Label htmlFor="description">Class Description</Label>
+						<Textarea
+							id="description"
 							name="description"
-							control={form.control}
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Class Description</FormLabel>
-									<FormControl>
-										<Textarea {...field} />
-									</FormControl>
-								</FormItem>
-							)}
+							placeholder="Class Description"
 						/>
-						<Button type="submit" size="lg">
-							Submit
-						</Button>
-					</form>
-				</Form>
+					</div>
+					<Button type="submit" className="w-full">
+						Submit
+					</Button>
+				</form>
 			</DialogContent>
 		</>
 	)

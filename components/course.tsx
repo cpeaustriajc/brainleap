077de4e0598a4ed1bef6093752dc6343fcdc34a5
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-02 15:21:41 +0800

    Improve course card look

--- components/course.tsx
+++ components/course.tsx
@@ -1,11 +1,30 @@
+import { Tables } from '@/lib/definitions'
 import Link from 'next/link'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from './ui/card'
+import { Button } from './ui/button'
 
-export function Course() {
+export function Course({ course }: { course: Tables<'classes'> }) {
 	return (
-		<Link href="/course/cpe314">
-			<div className="border w-64 h-64 rounded px-4 py-2">
-				<h2 className="font-bold">Course Title</h2>
-			</div>
-		</Link>
+		<Card>
+			<CardHeader>
+				<CardTitle>{course.class_name}</CardTitle>
+				<CardDescription>{course.class_description}</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<p>You have 0 assignments for this week</p>
+			</CardContent>
+			<CardFooter>
+				<Button className="w-full" asChild>
+					<Link href={`/course/${course.class_id}`}>View More</Link>
+				</Button>
+			</CardFooter>
+		</Card>
 	)
 }

1b07988b426935354db903f53cdaa660c937d553
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-09 14:15:51 +0800

    Use getProfile query

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,33 +1,16 @@
 import { SetupProfileForm } from '@/components/setup-profile-form'
-import { createClient } from '@/lib/supabase/server'
-import { cookies } from 'next/headers'
+import { getProfile } from '@/lib/queries'
 
 export default async function Page() {
-	const cookieStore = cookies()
-	const supabase = createClient(cookieStore)
+	const profile = await getProfile()
 
-	const {
-		data: { session },
-		error: sessionError,
-	} = await supabase.auth.getSession()
-
-	if (sessionError) {
-		throw sessionError
-	}
-
-	const { data, error } = await supabase
-		.from('profiles')
-		.select()
-		.eq('profile_id', session?.user.id ?? '')
-		.single()
-
-	if (error) {
-		throw error
+	if (!profile) {
+		throw new Error('Profile not found')
 	}
 
 	return (
 		<main>
-			<SetupProfileForm profile={data} />
+			<SetupProfileForm profile={profile} />
 		</main>
 	)
 }

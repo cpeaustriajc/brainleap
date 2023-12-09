7c90ef59659fa08715900dca1f9c1416863c499a
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-09 12:59:20 +0800

    Add header skeletoin

---
+++ components/header-skeleton.tsx
@@ -0,0 +1,14 @@
+export function HeaderSkeleton() {
+    return (
+        <header className="flex items-center justify-between px-7 py-4">
+            <div className="flex items-center gap-4">
+                <div className="w-10 h-10 rounded-full bg-gray-300 animate-pulse"></div>
+                <div className="w-10 h-10 rounded-full bg-gray-300 animate-pulse"></div>
+            </div>
+            <div className="flex items-center gap-4">
+                <div className="w-10 h-10 rounded-full bg-gray-300 animate-pulse"></div>
+                <div className="w-10 h-10 rounded-full bg-gray-300 animate-pulse"></div>
+            </div>
+        </header>
+    )
+}

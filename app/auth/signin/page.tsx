44adf5a9d4488aa4f958017ff4e32cd7706f8d72
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-09 12:57:45 +0800

    Remove footer

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,15 +1,13 @@
-import { Footer } from '@/components/footer'
 import { AuthForm } from '@/components/auth-form'
 
 export default function Page() {
 	return (
 		<>
-			<section className="min-h-[90svh] flex justify-center items-center">
+			<section className="min-h-[90lvh] flex justify-center items-center">
 				<div className="max-w-sm">
 					<AuthForm />
 				</div>
 			</section>
-			<Footer />
 		</>
 	)
 }

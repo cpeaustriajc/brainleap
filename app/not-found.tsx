6d4733ed5dfaf43955ea55b03d5599d95bbf6058
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-09 14:15:25 +0800

    Display a better not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
 	return (
-		<div>
-			<h1>404 - Page Not Found</h1>
+		<div className="flex flex-col h-[100svh] justify-center items-center">
+			<h1 className="text-2xl font-extrabold">No Courses Found</h1>
 		</div>
 	)
 }

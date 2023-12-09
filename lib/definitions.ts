c027b87f1357a0916ee5e77e4c024efaf51cd8f0
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-09 13:00:09 +0800

    Remove enum utility type

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -2,7 +2,3 @@ import type { Database } from './database.types'
 
 export type Tables<T extends keyof Database['public']['Tables']> =
 	Database['public']['Tables'][T]['Row']
-
-// TODO: Find a fix or delete
-export type Enums<T extends keyof Database['public']['Enums']> =
-	Database['public']['Enums'][T]['Enum']

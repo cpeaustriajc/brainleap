94f513388b8f9f70eb60ea3a12f1506f4e92477a
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-10 10:17:59 +0800

    Add newline between if and const declarations

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -5,6 +5,7 @@ export async function GET(req: Request) {
 	const { searchParams, origin } = new URL(req.url)
 	const code = searchParams.get('code')
 	const next = searchParams.get('next') ?? '/'
+
 	if (code) {
 		const cookieStore = cookies()
 		const supabase = createClient(cookieStore)

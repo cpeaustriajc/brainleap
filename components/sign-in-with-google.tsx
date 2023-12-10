927f31e532ecf2335152296633cefc9f441799f6
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-10 10:42:20 +0800

    Format files

--- components/sign-in-with-google.tsx
+++ components/sign-in-with-google.tsx
@@ -4,9 +4,7 @@ import { Button } from './ui/button'
 export function SignInWithGoogle() {
 	return (
 		<form action={signInWithGoogle} className="flex flex-col">
-			<Button type="submit">
-				Sign in with Google
-			</Button>
+			<Button type="submit">Sign in with Google</Button>
 		</form>
 	)
 }

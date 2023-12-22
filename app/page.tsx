9ad4582032ff822783571114ae62995330fbd70f
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-22 11:57:25 +0800

    Fix filtering for enrollments

--- app/page.tsx
+++ app/page.tsx
@@ -20,38 +20,31 @@ export default async function Page() {
 
 	const coursesData = getCourses()
 	const enrollmentsData = getEnrollments()
-	const profileData = await getProfile(user.id)
 
-	const [allCourses, enrollments, profile] = await Promise.all([
+	const [courses, enrollments] = await Promise.all([
 		coursesData,
 		enrollmentsData,
-		profileData,
 	])
 
-	if (!allCourses || !enrollments) {
+	if (!courses || !enrollments) {
 		return notFound()
 	}
 
-	const courses = allCourses.filter((course) => {
-		return enrollments.some((enrollment) => {
-			return (
-				enrollment.course_id === course.course_id &&
-				enrollment.user_id === profile.profile_id
-			)
-		})
-	})
-	console.log(courses)
-	if (!courses) {
-		notFound()
-	}
+	const studentsEnrolled = enrollments.filter(
+		(enrollment) => enrollment.user_id === user.id,
+	)
+
+	const filterCoursesEnrolled = courses.filter((course) =>
+		studentsEnrolled.find((enrollment) => enrollment.course_id === course.course_id),
+	)
 
 	return (
 		<AppShell>
 			<main>
 				<section className="flex px-7 gap-4">
 					<Suspense fallback={<CourseSkeleton />}>
-						{courses.map((course) => (
-							<Course key={course.course_id} course={course} />
+						{filterCoursesEnrolled.map((course) => (
+							<Course key={course?.course_id} course={course} />
 						))}
 					</Suspense>
 				</section>

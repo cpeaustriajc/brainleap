fd69def631b29378a06e83b01d575c5ccd49c7fd
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-12-28 10:58:32 +0800

    Squash migration

--- supabase/migrations/20231227061700_create_trigger_on_auth_user_created.sql
+++
@@ -1,3 +0,0 @@
-CREATE TRIGGER on_auth_user_created
-AFTER
-INSERT ON auth.users FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
--- supabase/migrations/20231227133246_remote_schema.sql
+++
@@ -1,3 +0,0 @@
-alter table "public"."outputs" alter column "submitted_at" set default (now() AT TIME ZONE 'utc'::text);
-
-

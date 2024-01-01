7246e2f165b171381deb4ba63e9201f61ca31f2b
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-01 11:44:28 +0800

    Remove react-label by radix to use react-aria-components' label

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,26 +1,14 @@
 'use client'
 
-import * as React from 'react'
-import * as LabelPrimitive from '@radix-ui/react-label'
-import { cva, type VariantProps } from 'class-variance-authority'
+import { cx } from '@/lib/cva.config'
+import { LabelProps, Label } from 'react-aria-components'
 
-import { cn } from '@/lib/utils'
 
-const labelVariants = cva(
-	'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
-)
-
-const Label = React.forwardRef<
-	React.ElementRef<typeof LabelPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-		VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-	<LabelPrimitive.Root
-		ref={ref}
-		className={cn(labelVariants(), className)}
+const _Label = (({ className, ...props }: LabelProps) => (
+	<Label
+		className={cx('text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70', className)}
 		{...props}
 	/>
 ))
-Label.displayName = LabelPrimitive.Root.displayName
 
-export { Label }
+export { _Label as Label }

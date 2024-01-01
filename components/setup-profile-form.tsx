92f0a24f20f631083a7ad8385d2ebc2e390a63e4
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-01 11:43:38 +0800

    Move setup profile to a separate actions file

--- components/setup-profile-form.tsx
+++ components/setup-profile-form.tsx
@@ -2,15 +2,12 @@ import { Button } from '@/components/ui/button'
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
 import { Tables } from '@/lib/database.types'
-import { createClient } from '@/lib/supabase/server'
+import { extractUsername } from '@/lib/utils'
 import { cx } from '@/lib/cva.config'
 import { ProfilePicture } from './profile-picture'
 import { Label } from './ui/label'
 import { RadioGroup, RadioGroupItem } from './ui/radio-group'
-import { cookies } from 'next/headers'
-import { revalidateTag } from 'next/cache'
-import { redirect } from 'next/navigation'
-import { profileSchema } from '@/lib/validations/profile'
+import { updateProfile } from '@/lib/actions/profile'
 
 export function SetupProfileForm({
 	profile,
@@ -19,85 +16,6 @@ export function SetupProfileForm({
 	profile: Tables<'profiles'>
 	message: string
 }) {
-	async function updateProfile(formData: FormData) {
-		'use server'
-		const cookieStore = cookies()
-		const supabase = createClient(cookieStore)
-		const date = new Date()
-		const updated_at = createPostgresTimestamp(date)
-
-		const values = profileSchema.safeParse({
-			biography: formData.get('biography'),
-			email: formData.get('email'),
-			full_name: formData.get('full_name'),
-			role: formData.get('role'),
-			section: formData.get('section'),
-			university: formData.get('university'),
-			username: formData.get('username'),
-			program: formData.get('program'),
-			position: formData.get('position'),
-		})
-
-		const {
-			data: { session },
-			error: sessionError,
-		} = await supabase.auth.getSession()
-
-		if (sessionError) {
-			throw sessionError
-		}
-
-		if (!session) {
-			redirect('/auth/signin')
-		}
-
-		const {
-			user: { id },
-		} = session
-
-		if (!values.success) {
-			throw values.error
-		}
-
-		if (values.data.role === 'student') {
-			const { error } = await supabase
-				.from('profiles')
-				.update({
-					full_name: values.data.full_name,
-					username: values.data.username,
-					biography: values.data.biography,
-					program: values.data.program,
-					section: values.data.section,
-					university: values.data.university,
-					email: values.data.email,
-					updated_at,
-				})
-				.eq('profile_id', id)
-
-			if (error) {
-				throw error
-			}
-		} else if (values.data.role === 'instructor') {
-			const { error } = await supabase
-				.from('profiles')
-				.update({
-					avatar_url: values.data.avatar_url,
-					full_name: values.data.full_name,
-					username: values.data.username,
-					biography: values.data.biography,
-					position: values.data.position,
-					email: values.data.email,
-					role: values.data.role,
-					updated_at,
-				})
-				.eq('profile_id', id)
-
-			if (error) {
-				throw error
-			}
-		}
-		revalidateTag('profile')
-	}
 
 	const action = async (formData: FormData) => {
 		'use server'
---
+++ lib/actions/profile.ts
@@ -0,0 +1,86 @@
+import { cookies } from "next/headers"
+import { createClient } from "@/lib/supabase/server"
+import { profileSchema } from "@/lib/validations/profile"
+import { createPostgresTimestamp } from "../utils"
+import { redirect } from "next/navigation"
+import { revalidateTag } from "next/cache"
+
+export async function updateProfile(formData: FormData) {
+    'use server'
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+    const date = new Date()
+    const updated_at = createPostgresTimestamp(date)
+
+    const values = profileSchema.safeParse({
+        biography: formData.get('biography'),
+        email: formData.get('email'),
+        full_name: formData.get('full_name'),
+        role: formData.get('role'),
+        section: formData.get('section'),
+        university: formData.get('university'),
+        username: formData.get('username'),
+        program: formData.get('program'),
+        position: formData.get('position'),
+    })
+
+    const {
+        data: { session },
+        error: sessionError,
+    } = await supabase.auth.getSession()
+
+    if (sessionError) {
+        throw sessionError
+    }
+
+    if (!session) {
+        redirect('/auth/signin')
+    }
+
+    const {
+        user: { id },
+    } = session
+
+    if (!values.success) {
+        throw values.error
+    }
+
+    if (values.data.role === 'student') {
+        const { error } = await supabase
+            .from('profiles')
+            .update({
+                full_name: values.data.full_name,
+                username: values.data.username,
+                biography: values.data.biography,
+                program: values.data.program,
+                section: values.data.section,
+                university: values.data.university,
+                email: values.data.email,
+                updated_at,
+            })
+            .eq('profile_id', id)
+
+        if (error) {
+            throw error
+        }
+    } else if (values.data.role === 'instructor') {
+        const { error } = await supabase
+            .from('profiles')
+            .update({
+                avatar_url: values.data.avatar_url,
+                full_name: values.data.full_name,
+                username: values.data.username,
+                biography: values.data.biography,
+                position: values.data.position,
+                email: values.data.email,
+                role: values.data.role,
+                updated_at,
+            })
+            .eq('profile_id', id)
+
+        if (error) {
+            throw error
+        }
+    }
+    revalidateTag('profile')
+}

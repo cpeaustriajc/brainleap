68fa477312d6988ba9572e2abd35b77419f5c0c6
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-05 18:40:41 +0800

    Add react-aria and tailwindcss plugin for react-aria

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -67,5 +67,8 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require('tailwindcss-animate')],
+	plugins: [
+		require('tailwindcss-animate'),
+		require('tailwindcss-react-aria-components')({ prefix: 'rac' }),
+	],
 }

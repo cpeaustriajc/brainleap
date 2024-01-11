7765e3864eee971b63321e673257ecd36144d67c
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-11 19:37:37 +0800

    Remove unused route handlers

--- app/api/announcement/ids/route.ts
+++
@@ -1,9 +0,0 @@
-import { getAnnouncementIds } from '@/lib/queries/announcement'
-
-export async function GET() {
-	const announcementIds = await getAnnouncementIds()
-
-	return Response.json(
-		announcementIds.map((announcement_id) => announcement_id),
-	)
-}
--- app/api/course/ids/route.ts
+++
@@ -1,7 +0,0 @@
-import { getCourseIds } from '@/lib/queries/course'
-
-export async function GET() {
-	const courseIds = await getCourseIds()
-
-	return Response.json(courseIds.map((courseId) => courseId))
-}

4765d6298dda487f435b3d9b2ea291fe89b10ea6
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-12 11:36:49 +0800

    Don't use AppShell in not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,7 @@
-import AppShell from '@/components/app-shell'
-
 export default function NotFound() {
 	return (
-		<AppShell>
-			<div className="flex flex-col h-[100svh] justify-center items-center">
-				<h1 className="text-2xl font-extrabold">Not Found</h1>
-			</div>
-		</AppShell>
+		<div className="flex flex-col h-[100svh] justify-center items-center">
+			<h1 className="text-2xl font-extrabold">Not Found</h1>
+		</div>
 	)
 }

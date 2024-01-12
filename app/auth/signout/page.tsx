6d52a39e8067b3d33411f6b016f20d2a7ed370b5
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-12 21:00:54 +0800

    Make sure that user has an option to cancel signout

--- app/auth/signout/page.tsx
+++ app/auth/signout/page.tsx
@@ -1,14 +1,26 @@
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function Page() {
 	return (
-		<div className='flex flex-col justify-center items-center gap-4'>
+		<div className="flex flex-col justify-center items-center gap-4">
 			<h2 className="text-4xl tracking-tight">
 				Are you sure you want to sign out?
 			</h2>
-			<form action="/api/auth/signout" method="POST">
-				<Button type="submit" size="lg">Log Out</Button>
+			<form
+				action="/api/auth/signout"
+				className="grid gap-2"
+				method="POST"
+			>
+				<Button type="submit" size="lg">
+					Log Out
+				</Button>
 			</form>
+			<Link href="/">
+				<Button variant="ghost" size="lg">
+					Cancel
+				</Button>
+			</Link>
 		</div>
 	)
 }

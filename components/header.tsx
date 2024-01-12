15bde79df6c6fd64d0c96d1e0947febc85060721
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-12 14:06:04 +0800

    Use react-aria's dropdown menu

--- components/header.tsx
+++ components/header.tsx
@@ -4,43 +4,11 @@ import Link from 'next/link'
 import { Button, buttonVariants } from './ui/button'
 import { PersonIcon, PlusCircledIcon } from '@radix-ui/react-icons'
 import Image from 'next/image'
-import dynamic from 'next/dynamic'
-import { DropdownMenuItem } from '@radix-ui/react-dropdown-menu'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Tables } from '@/lib/database.types'
 import { useUpload } from '@/hooks/use-upload'
 import humanId from 'human-id'
-
-const DropdownMenu = dynamic(
-	() => import('./ui/dropdown-menu').then((mod) => mod.DropdownMenu),
-	{
-		ssr: false,
-		loading: () => (
-			<Button
-				size="icon"
-				variant="secondary"
-				className="animate-pulse"
-			></Button>
-		),
-	},
-)
-const DropdownMenuContent = dynamic(
-	() => import('./ui/dropdown-menu').then((mod) => mod.DropdownMenuContent),
-	{ ssr: false },
-)
-const DropdownMenuTrigger = dynamic(
-	() => import('./ui/dropdown-menu').then((mod) => mod.DropdownMenuTrigger),
-	{
-		ssr: false,
-		loading: () => (
-			<Button
-				size="icon"
-				variant="secondary"
-				className="animate-pulse"
-			></Button>
-		),
-	},
-)
+import * as ReactAria from 'react-aria-components'
 
 export function Header({
 	profile,
@@ -79,95 +47,97 @@ export function Header({
 				</div>
 				<div className="flex items-center gap-2">
 					{profile && (
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button variant="ghost" size="icon">
-									<span className="sr-only">
-										Open Class Menu
-									</span>
-									<PlusCircledIcon className="w-6 h-6" />
-								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent align="end">
-								{profile.role === 'instructor' && (
-									<DropdownMenuItem>
+						<ReactAria.MenuTrigger>
+							<Button
+								aria-label="Class Menu"
+								variant="ghost"
+								size="icon"
+							>
+								<span className="sr-only">Open Class Menu</span>
+								<PlusCircledIcon className="w-6 h-6" />
+							</Button>
+							<ReactAria.Popover placement="bottom right">
+								<ReactAria.Menu>
+									{profile.role === 'instructor' && (
+										<ReactAria.MenuItem id="create-course">
+											<Link
+												href="/create/course"
+												className={buttonVariants({
+													size: 'lg',
+													variant: 'ghost',
+												})}
+											>
+												Create Course
+											</Link>
+										</ReactAria.MenuItem>
+									)}
+									<ReactAria.MenuItem id="join-course">
 										<Link
-											href="/create/course"
+											href="/join/course"
 											className={buttonVariants({
 												size: 'lg',
 												variant: 'ghost',
 											})}
 										>
-											Create Course
+											Enroll Course
 										</Link>
-									</DropdownMenuItem>
-								)}
-								<DropdownMenuItem>
+									</ReactAria.MenuItem>
+								</ReactAria.Menu>
+							</ReactAria.Popover>
+						</ReactAria.MenuTrigger>
+					)}
+					<ReactAria.MenuTrigger>
+						<Button variant="ghost" size="icon">
+							<span className="sr-only">Open User Menu</span>
+							<Avatar className="h-8 w-8">
+								<AvatarImage src={fileUrl || undefined} />
+								<AvatarFallback>
+									<PersonIcon />
+								</AvatarFallback>
+							</Avatar>
+						</Button>
+						<ReactAria.Popover placement="bottom right">
+							<ReactAria.Menu>
+								{profile ? (
+									<>
+										<ReactAria.MenuItem>
+											<Link
+												href="/profile"
+												className={buttonVariants({
+													variant: 'link',
+												})}
+											>
+												Profile
+											</Link>
+										</ReactAria.MenuItem>
+										<ReactAria.MenuItem>
+											<form
+												action="/api/auth/signout"
+												method="POST"
+											>
+												<Button
+													type="submit"
+													variant="link"
+												>
+													Log Out
+												</Button>
+											</form>
+										</ReactAria.MenuItem>
+									</>
+								) : (
 									<Link
-										href="/join/course"
+										href="/auth/signin"
 										className={buttonVariants({
-											size: 'lg',
-											variant: 'ghost',
+											variant: 'link',
+											className: 'w-full',
 										})}
 									>
-										Enroll Course
+										Sign in
 									</Link>
-								</DropdownMenuItem>
-							</DropdownMenuContent>
-						</DropdownMenu>
-					)}
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<Button variant="ghost" size="icon">
-								<span className="sr-only">Open User Menu</span>
-								<Avatar className="h-8 w-8">
-									<AvatarImage src={fileUrl || undefined} />
-									<AvatarFallback>
-										<PersonIcon />
-									</AvatarFallback>
-								</Avatar>
-							</Button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent side="bottom" align="end">
-							{profile ? (
-								<>
-									<DropdownMenuItem>
-										<Link
-											href="/profile"
-											className={buttonVariants({
-												variant: 'link',
-											})}
-										>
-											Profile
-										</Link>
-									</DropdownMenuItem>
-									<DropdownMenuItem>
-										<form
-											action="/api/auth/signout"
-											method="POST"
-										>
-											<Button
-												type="submit"
-												variant="link"
-											>
-												Log Out
-											</Button>
-										</form>
-									</DropdownMenuItem>
-								</>
-							) : (
-								<Link
-									href="/auth/signin"
-									className={buttonVariants({
-										variant: 'link',
-										className: 'w-full',
-									})}
-								>
-									Sign in
-								</Link>
-							)}
-						</DropdownMenuContent>
-					</DropdownMenu>
+								)}
+							</ReactAria.Menu>
+						</ReactAria.Popover>
+					</ReactAria.MenuTrigger>
 				</div>
 			</div>
 		</header>

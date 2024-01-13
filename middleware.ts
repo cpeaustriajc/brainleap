772f8a921d419b27ea75bc10a0c99ce1142fae11
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-13 08:45:39 +0800

    Revert "Add some more middleware matchers"
    This reverts commit c51a4578437ab789306b20b0f2d24a32c751913a.

--- middleware.ts
+++ middleware.ts
@@ -31,5 +31,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ['/((?!.*\\..*|_next).*)', "/(api|trpc)(.*)", '/', '/profile'],
+	matcher: ['/', '/profile'],
 }

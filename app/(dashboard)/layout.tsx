76da2b06962b03ac55104c50d9494e67ae25b0f4
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-20 21:07:21 +0800

    Temporarily remove ai prompt

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,9 @@
-import { AIPrompt } from '@/components/ai-prompt'
 import AppShell from '@/components/app-shell'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<AppShell>
 			{children}
-			<AIPrompt />
 		</AppShell>
 	)
 }

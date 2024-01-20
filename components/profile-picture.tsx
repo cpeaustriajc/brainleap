532b05cf51b1fe9f22894a28a5f0eca27a1d3fbe
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-20 21:10:12 +0800

    Tweak the profile picture component for setup-profile-form

--- components/profile-picture.tsx
+++ components/profile-picture.tsx
@@ -4,7 +4,7 @@ import { useTransition } from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Tables } from '@/lib/database.types'
 import { Button } from './ui/button'
-import {  UserRoundIcon } from 'lucide-react'
+import { UserRoundIcon } from 'lucide-react'
 import * as ReactAria from 'react-aria-components'
 import { uploadAvatar } from '@/lib/actions/profile'
 
@@ -17,7 +17,29 @@ export function ProfilePicture({ url, size }: Props) {
 	const [isPending, startTransition] = useTransition()
 
 	return (
-		<div className="space-y-4">
+		<div className="flex justify-between gap-4">
+			<div className="grid">
+				<div>
+					<strong>Profile Picture</strong>
+					<p>This is your profile picture</p>
+				</div>
+				<ReactAria.FileTrigger
+					onSelect={(e) => {
+						if (e) {
+							const formData = new FormData()
+							formData.set('avatar', e[0])
+							startTransition(() => {
+								uploadAvatar(formData)
+							})
+						}
+					}}
+					acceptedFileTypes={['image/*']}
+				>
+					<Button isDisabled={isPending}>
+						{isPending ? 'Uploading...' : 'Change Profile Picture'}
+					</Button>
+				</ReactAria.FileTrigger>
+			</div>
 			<Avatar style={{ width: size, height: size }}>
 				<AvatarImage
 					src={url!}
@@ -26,25 +48,11 @@ export function ProfilePicture({ url, size }: Props) {
 					style={{ width: size, height: size }}
 				/>
 				<AvatarFallback style={{ width: size, height: size }}>
-					<UserRoundIcon style={{ width: size / 2, height: size / 2 }} />
+					<UserRoundIcon
+						style={{ width: size / 2, height: size / 2 }}
+					/>
 				</AvatarFallback>
 			</Avatar>
-			<ReactAria.FileTrigger
-				onSelect={(e) => {
-					if (e) {
-						const formData = new FormData()
-						formData.set('avatar', e[0])
-						startTransition(() => {
-							uploadAvatar(formData)
-						})
-					}
-				}}
-				acceptedFileTypes={['image/*']}
-			>
-				<Button isDisabled={isPending}>
-					{isPending ? 'Uploading...' : 'Change Profile Picture'}
-				</Button>
-			</ReactAria.FileTrigger>
 		</div>
 	)
 }

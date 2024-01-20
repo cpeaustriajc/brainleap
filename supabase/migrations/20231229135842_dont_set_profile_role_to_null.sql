e5394029f89af26bb996b2cbc8819acdf6a1f22a
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-20 13:17:34 +0800

    Reset migrations (?)

--- supabase/migrations/20231229135842_dont_set_profile_role_to_null.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "public"."profiles" ALTER COLUMN "role" SET NOT NULL;
--- supabase/migrations/20231229140246_remote_schema.sql
+++
@@ -1,7 +0,0 @@
-alter table "storage"."buckets" drop constraint "buckets_owner_fkey";
-
-alter table "storage"."buckets" add column "owner_id" text;
-
-alter table "storage"."objects" add column "owner_id" text;
-
-

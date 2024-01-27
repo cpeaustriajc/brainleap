3e8d7b3994f5e7e791efe3f2defaf984a5666056
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-27 13:32:03 +0800

    Remove experimental options that doesn't work properly

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const config = {
-	experimental: {
-		optimizePackageImports: ['react-aria-components'],
-	},
 	logging: {
 		fetches: { fullUrl: true },
 	},

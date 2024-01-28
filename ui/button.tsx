b3e84a0f4fb5f71727e2537393000f32473039fc
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-28 21:58:38 +0800

    Use default cursor state on buttons
    reference: https://www.w3.org/TR/CSS21/ui.html#propdef-cursor

--- ui/button.tsx
+++ ui/button.tsx
@@ -2,7 +2,7 @@ import { cx } from '@/lib/cva.config'
 
 export const button = cx(
 	// Base
-	'inline-flex items-center justify-center rounded-md text-sm font-medium gap-x-1 w-full ring-offset-background transition-colors',
+	'inline-flex cursor-default items-center justify-center rounded-md text-sm font-medium gap-x-1 w-full ring-offset-background transition-colors',
 	'bg-emerald-700 text-emerald-50 hover:bg-emerald-700/90 border border-emerald-600',
 	// Disabled
 	'disabled:pointer-events-none disabled:opacity-50',

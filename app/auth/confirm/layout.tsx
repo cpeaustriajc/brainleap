60614937ea7932fc65d07d048e2104c372b11a06
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-01-28 20:17:31 +0800

    Make layouts more granular

---
+++ app/auth/confirm/layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ConfirmPageLayout({
+	children,
+}: { children: React.ReactNode }) {
+	return <main className="grid place-items-center h-dvh">{children}</main>
+}
--- app/auth/confirm/page.tsx
+++ app/auth/confirm/page.tsx
@@ -1,31 +1,19 @@
+import { CheckCircleIcon, CheckIcon } from 'lucide-react'
+import React from 'react'
+
 export default function Page() {
 	return (
-		<section>
-			<div>
-				<div>
-					<h2>Magic Link Sent</h2>
-					<p>
-						We have sent a magic link to your email address. Please check your
-						inbox and click on the link to proceed.
-					</p>
-				</div>
-				<div>
-					<div>
-						<div>
-							<svg aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-								<path
-									clipRule="evenodd"
-									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-									fillRule="evenodd"
-								/>
-							</svg>
-						</div>
-						<div>
-							<p>Magic link sent successfully!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		<article className='rounded-md border border-stone-200 dark:border-stone-800 aspect-square gap-y-12 grid content-center p-4'>
+			<header className="grid grid-flow-col place-items-center justify-center gap-x-2">
+				<h1 className="text-4xl font-bold text-green-600">Magic Link Sent</h1>
+				<CheckCircleIcon className="text-green-600 size-8" />
+			</header>
+			<footer>
+				<p className="max-w-sm text-center">
+					We have sent a magic link to your email address. Please check your
+					inbox and click on the link to proceed.
+				</p>
+			</footer>
+		</article>
 	)
 }

42d59f98c283cfa646f45f61a5be97f8c97a3683
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-01 13:05:56 +0800

    Update database types

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -7,6 +7,31 @@ export type Json =
 	| Json[]
 
 export interface Database {
+	graphql_public: {
+		Tables: {
+			[_ in never]: never
+		}
+		Views: {
+			[_ in never]: never
+		}
+		Functions: {
+			graphql: {
+				Args: {
+					operationName?: string
+					query?: string
+					variables?: Json
+					extensions?: Json
+				}
+				Returns: Json
+			}
+		}
+		Enums: {
+			[_ in never]: never
+		}
+		CompositeTypes: {
+			[_ in never]: never
+		}
+	}
 	public: {
 		Tables: {
 			announcements: {
@@ -226,7 +251,7 @@ export interface Database {
 					avatar_url: string | null
 					biography: string | null
 					email: string
-					full_name: string | null
+					full_name: string
 					position: string | null
 					profile_id: string
 					program: string | null
@@ -234,13 +259,13 @@ export interface Database {
 					section: string | null
 					university: string | null
 					updated_at: string | null
-					username: string | null
+					username: string
 				}
 				Insert: {
 					avatar_url?: string | null
 					biography?: string | null
 					email: string
-					full_name?: string | null
+					full_name: string
 					position?: string | null
 					profile_id: string
 					program?: string | null
@@ -248,13 +273,13 @@ export interface Database {
 					section?: string | null
 					university?: string | null
 					updated_at?: string | null
-					username?: string | null
+					username: string
 				}
 				Update: {
 					avatar_url?: string | null
 					biography?: string | null
 					email?: string
-					full_name?: string | null
+					full_name?: string
 					position?: string | null
 					profile_id?: string
 					program?: string | null
@@ -262,7 +287,7 @@ export interface Database {
 					section?: string | null
 					university?: string | null
 					updated_at?: string | null
-					username?: string | null
+					username?: string
 				}
 				Relationships: [
 					{
@@ -289,15 +314,192 @@ export interface Database {
 			[_ in never]: never
 		}
 	}
+	storage: {
+		Tables: {
+			buckets: {
+				Row: {
+					allowed_mime_types: string[] | null
+					avif_autodetection: boolean | null
+					created_at: string | null
+					file_size_limit: number | null
+					id: string
+					name: string
+					owner: string | null
+					owner_id: string | null
+					public: boolean | null
+					updated_at: string | null
+				}
+				Insert: {
+					allowed_mime_types?: string[] | null
+					avif_autodetection?: boolean | null
+					created_at?: string | null
+					file_size_limit?: number | null
+					id: string
+					name: string
+					owner?: string | null
+					owner_id?: string | null
+					public?: boolean | null
+					updated_at?: string | null
+				}
+				Update: {
+					allowed_mime_types?: string[] | null
+					avif_autodetection?: boolean | null
+					created_at?: string | null
+					file_size_limit?: number | null
+					id?: string
+					name?: string
+					owner?: string | null
+					owner_id?: string | null
+					public?: boolean | null
+					updated_at?: string | null
+				}
+				Relationships: []
+			}
+			migrations: {
+				Row: {
+					executed_at: string | null
+					hash: string
+					id: number
+					name: string
+				}
+				Insert: {
+					executed_at?: string | null
+					hash: string
+					id: number
+					name: string
+				}
+				Update: {
+					executed_at?: string | null
+					hash?: string
+					id?: number
+					name?: string
+				}
+				Relationships: []
+			}
+			objects: {
+				Row: {
+					bucket_id: string | null
+					created_at: string | null
+					id: string
+					last_accessed_at: string | null
+					metadata: Json | null
+					name: string | null
+					owner: string | null
+					owner_id: string | null
+					path_tokens: string[] | null
+					updated_at: string | null
+					version: string | null
+				}
+				Insert: {
+					bucket_id?: string | null
+					created_at?: string | null
+					id?: string
+					last_accessed_at?: string | null
+					metadata?: Json | null
+					name?: string | null
+					owner?: string | null
+					owner_id?: string | null
+					path_tokens?: string[] | null
+					updated_at?: string | null
+					version?: string | null
+				}
+				Update: {
+					bucket_id?: string | null
+					created_at?: string | null
+					id?: string
+					last_accessed_at?: string | null
+					metadata?: Json | null
+					name?: string | null
+					owner?: string | null
+					owner_id?: string | null
+					path_tokens?: string[] | null
+					updated_at?: string | null
+					version?: string | null
+				}
+				Relationships: [
+					{
+						foreignKeyName: 'objects_bucketId_fkey'
+						columns: ['bucket_id']
+						isOneToOne: false
+						referencedRelation: 'buckets'
+						referencedColumns: ['id']
+					},
+				]
+			}
+		}
+		Views: {
+			[_ in never]: never
+		}
+		Functions: {
+			can_insert_object: {
+				Args: {
+					bucketid: string
+					name: string
+					owner: string
+					metadata: Json
+				}
+				Returns: undefined
+			}
+			extension: {
+				Args: {
+					name: string
+				}
+				Returns: string
+			}
+			filename: {
+				Args: {
+					name: string
+				}
+				Returns: string
+			}
+			foldername: {
+				Args: {
+					name: string
+				}
+				Returns: unknown
+			}
+			get_size_by_bucket: {
+				Args: Record<PropertyKey, never>
+				Returns: {
+					size: number
+					bucket_id: string
+				}[]
+			}
+			search: {
+				Args: {
+					prefix: string
+					bucketname: string
+					limits?: number
+					levels?: number
+					offsets?: number
+					search?: string
+					sortcolumn?: string
+					sortorder?: string
+				}
+				Returns: {
+					name: string
+					id: string
+					updated_at: string
+					created_at: string
+					last_accessed_at: string
+					metadata: Json
+				}[]
+			}
+		}
+		Enums: {
+			[_ in never]: never
+		}
+		CompositeTypes: {
+			[_ in never]: never
+		}
+	}
 }
 
 export type Tables<
 	PublicTableNameOrOptions extends
 		| keyof (Database['public']['Tables'] & Database['public']['Views'])
 		| { schema: keyof Database },
-	TableName extends PublicTableNameOrOptions extends {
-		schema: keyof Database
-	}
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
 		? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
 				Database[PublicTableNameOrOptions['schema']]['Views'])
 		: never = never,
@@ -322,9 +524,7 @@ export type TablesInsert<
 	PublicTableNameOrOptions extends
 		| keyof Database['public']['Tables']
 		| { schema: keyof Database },
-	TableName extends PublicTableNameOrOptions extends {
-		schema: keyof Database
-	}
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
 		? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
 		: never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
@@ -345,9 +545,7 @@ export type TablesUpdate<
 	PublicTableNameOrOptions extends
 		| keyof Database['public']['Tables']
 		| { schema: keyof Database },
-	TableName extends PublicTableNameOrOptions extends {
-		schema: keyof Database
-	}
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
 		? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
 		: never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }

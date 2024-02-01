6d77bc4fecd6b3f214464cc715ed51f5283453b3
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-01 20:47:13 +0800

    Separate server client and action client for supabase

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,4 +1,4 @@
-import { type CookieOptions, createServerClient } from '@supabase/ssr'
+import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { Database } from '../database.types'
 
@@ -10,24 +10,6 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
 			cookies: {
 				get(name: string) {
 					return cookieStore.get(name)?.value
-				},
-				set(name: string, value: string, options: CookieOptions) {
-					try {
-						cookieStore.set({ name, value, ...options })
-					} catch (error) {
-						// The `set` method was called from a Server Component.
-						// This can be ignored if you have middleware refreshing
-						// user sessions.
-					}
-				},
-				remove(name: string, options: CookieOptions) {
-					try {
-						cookieStore.set({ name, value: '', ...options })
-					} catch (error) {
-						// The `delete` method was called from a Server Component.
-						// This can be ignored if you have middleware refreshing
-						// user sessions.
-					}
 				},
 			},
 		},

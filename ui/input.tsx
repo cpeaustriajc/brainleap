22c6c4436aabacaf86c75025270d749bff3f41e4
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-02 20:24:29 +0800

    Add styles for the file button inside the input

--- ui/input.tsx
+++ ui/input.tsx
@@ -1,8 +1,9 @@
 import { cx } from '@/lib/cva.config'
 
 export const input = cx(
-	'flex h-9 w-full rounded-lg border bg-stone-800 border-stone-700 px-2 py-2 text-sm',
+	'flex h-9 w-full rounded-lg border bg-stone-800 border-stone-700 text-sm px-4 py-2',
 	'placeholder:text-stone-400 placeholder:font-medium',
 	'disabled:cursor-not-allowed disabled:opacity-50',
 	'focus-visible:outline focus-visible:outline-2 focus-visible:outline-emerald-400',
+	'file:bg-stone-800 file:px-2 file:h-full file:rounded file:border file:border-solid hover:file:bg-stone-700 file:transition-colors duration-500 file:border-stone-600',
 )

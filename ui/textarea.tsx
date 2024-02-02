c431dbbab8448c6ec3d8b2de6860c9680c24b010
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-02 20:20:57 +0800

    Add textarea styles

---
+++ ui/textarea.tsx
@@ -0,0 +1,6 @@
+import { cx } from '@/lib/cva.config'
+
+export const textarea = cx(
+	'inline-flex dark:bg-stone-800 border  border-stone-700 rounded',
+	'focus-visible:outline focus-visible:outline-2 focus-visible:outline-stone-700',
+)

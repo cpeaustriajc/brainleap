ef85051f23df1bd4e5909978a0fe4c7124b85939
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-03 13:12:25 +0800

    Update styles for button and textarea

--- ui/button.tsx
+++ ui/button.tsx
@@ -2,8 +2,9 @@ import { cx } from '@/lib/cva.config'
 
 export const button = cx(
 	// Base
-	'inline-flex cursor-default items-center justify-center rounded-md text-sm font-medium gap-x-1 w-full ring-offset-background transition-colors',
-	'bg-emerald-700 text-emerald-50 hover:bg-emerald-700/90 border border-emerald-600',
+	'inline-flex cursor-default items-center justify-center border rounded-md text-sm font-medium gap-x-1 w-full ring-offset-background transition-colors',
+	' text-emerald-50 bg-emerald-500 hover:bg-emerald-500/90 border-emerald-400',
+	'dark:bg-emerald-700 dark:hover:bg-emerald-700/90 dark:border-emerald-600',
 	// Disabled
 	'disabled:pointer-events-none disabled:opacity-50',
 	// Focus

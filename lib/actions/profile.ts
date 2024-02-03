2cd19608f31afb97136987e68081a51d1761ec2f
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-03 14:46:29 +0800

    Add more granular updates to the profile

--- lib/actions/profile.ts
+++ lib/actions/profile.ts
@@ -1,11 +1,7 @@
 'use server'
 
 import { createClient } from '@/lib/supabase/action'
-import {
-	fullNameSchema,
-	profileSchema,
-	usernameSchema,
-} from '@/lib/validations/profile'
+import { fullNameSchema, usernameSchema } from '@/lib/validations/profile'
 import { revalidatePath } from 'next/cache'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
@@ -87,7 +83,6 @@ export async function updateUsername(formData: FormData) {
 	}
 
 	revalidatePath('/profile')
-	redirect('/profile/setup/name')
 }
 
 export async function updateName(formData: FormData) {
@@ -121,5 +116,70 @@ export async function updateName(formData: FormData) {
 	}
 
 	revalidatePath('/profile')
-	redirect('/profile/setup/avatar')
+}
+
+export async function updateUniversity(formData: FormData) {
+	const cookieStore = cookies()
+	const supabase = createClient(cookieStore)
+
+	const university = formData.get('university') as string
+
+	const {
+		data: { session },
+	} = await supabase.auth.getSession()
+
+	if (!session) {
+		redirect('/auth/signin')
+	}
+
+	const { error } = await supabase
+		.from('profiles')
+		.update({
+			university: university,
+			updated_at: new Date().toISOString(),
+		})
+		.eq('profile_id', session.user.id)
+
+	if (error) {
+		throw error
+	}
+
+	revalidatePath('/profile')
+}
+
+export async function updateSection(formData: FormData) {
+	const cookieStore = cookies()
+	const supabase = createClient(cookieStore)
+	// const res = profileSchema.safeParse({
+	// 	section: formData.get('section'),
+	// })
+	const section = formData.get('section') as string
+
+	const {
+		data: { session },
+	} = await supabase.auth.getSession()
+
+	if (!session) {
+		redirect('/auth/signin')
+	}
+
+	// if (!res.success) {
+	// 	return {
+	// 		error: res.error,
+	// 	}
+	// }
+
+	const { error } = await supabase
+		.from('profiles')
+		.update({
+			section: section,
+			updated_at: new Date().toISOString(),
+		})
+		.eq('profile_id', session.user.id)
+
+	if (error) {
+		throw error
+	}
+
+	revalidatePath('/profile')
 }

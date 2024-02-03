695230dafd8f4e6c1ae381550a9650546a1510ad
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-03 01:24:14 +0000

    Add new styles for input and link

--- ui/input.tsx
+++ ui/input.tsx
@@ -1,7 +1,9 @@
 import { cx } from '@/lib/cva.config'
 
 export const input = cx(
-	'flex h-9 w-full rounded-lg border bg-stone-800 border-stone-700 text-sm px-4 py-2',
+	'flex h-9 w-full rounded-lg border text-sm px-4 py-2',
+	'bg-stone-200 border-stone-400',
+	'dark:bg-stone-800 dark:border-stone-700',
 	'placeholder:text-stone-400 placeholder:font-medium',
 	'disabled:cursor-not-allowed disabled:opacity-50',
 	'focus-visible:outline focus-visible:outline-2 focus-visible:outline-emerald-400',
--- ui/link.tsx
+++ ui/link.tsx
@@ -1,6 +1,8 @@
 import { cx } from '@/lib/cva.config'
 
 export const link = cx(
-	'w-full text-center font-medium underline underline-offset-4 text-green-400 transition-colors duration-500',
-	'hover:text-green-200',
+	'w-full text-center font-medium underline underline-offset-4',
+	'text-green-600',
+	' transition-colors duration-500',
+	'hover:text-green-500',
 )

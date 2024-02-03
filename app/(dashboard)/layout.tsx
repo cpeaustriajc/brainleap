d7468a157155b26ca48b8747d81993e9e9217e15
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-03 13:08:35 +0800

    Use header to go back to landing page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -57,9 +57,9 @@ export default async function DashboardRootLayout({
 	return (
 		<html dir="ltr" lang="en">
 			<body className="grid grid-cols-[minmax(auto,25%),1fr] grid-rows-[auto,auto,1fr] bg-stone-100 dark:bg-stone-800 h-dvh">
-				<header className="flex items-center px-4 bg-stone-100 py-2 col-span-2">
-					<h1 className="text-xl">
-						<Link href="/dashboard">Brainleap 🧠</Link>
+				<header className="flex justify-between items-center px-4 bg-stone-100 dark:bg-stone-800 py-2 col-span-2">
+					<h1 className="pl-2 text-xl">
+						<Link href="/">Brainleap 🧠</Link>
 					</h1>
 				</header>
 				<Tabs className="col-start-1 px-2 row-span-3 bg-stone-100">

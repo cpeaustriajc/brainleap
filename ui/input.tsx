65b8c82d486ab382b4dcd7434c6cdf98e3e9bb02
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-07 23:02:35 +0800

    Init label, form and input components from shadcn

--- ui/input.tsx
+++ ui/input.tsx
@@ -1,13 +1,28 @@
-import { cx } from '@/lib/cva.config'
+import * as React from "react"
 
-export const input = cx(
-	'flex h-9 w-full rounded-lg border text-sm px-4 py-2',
-	'bg-stone-200 border-stone-400',
-	'dark:bg-stone-800 dark:border-stone-700',
-	'placeholder:text-stone-400 placeholder:font-medium',
-	'disabled:cursor-not-allowed disabled:opacity-50',
-	'focus-visible:outline focus-visible:outline-2 focus-visible:outline-emerald-400',
-	'file:bg-stone-200 file:px-2 file:h-full file:rounded file:border file:border-solid file:transition-colors file:duration-500 file:border-stone-400',
-	'file:dark:bg-stone-800 file:dark:border-stone-700',
-	'hover:dark:file:bg-stone-700 hover:file:bg-stone-300',
+import { cva, cx } from "@/lib/cva.config"
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
+
+export const input = cva({
+  base: "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+})
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cx(input,
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
 )
+Input.displayName = "Input"
+
+export { Input }
--- ui/label.tsx
+++ ui/label.tsx
@@ -1,5 +1,26 @@
-import { cx } from '@/lib/cva.config'
+"use client"
 
-export const label = cx(
-	'font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
-)
+import * as LabelPrimitive from "@radix-ui/react-label"
+import { type VariantProps } from "cva"
+import * as React from "react"
+
+import { cva, cx } from "@/lib/cva.config"
+
+const label = cva({
+  base: "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+})
+
+const Label = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+  VariantProps<typeof label>
+>(({ className, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cx(label(), className)}
+    {...props}
+  />
+))
+Label.displayName = LabelPrimitive.Root.displayName
+
+export { Label }

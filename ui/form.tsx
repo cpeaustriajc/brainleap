10768eca463592ad5e1d51049ef489b98e929626
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-07 19:14:24 +0800

    Add back form.tsx

---
+++ ui/form.tsx
@@ -0,0 +1,8 @@
+import { cx } from '@/lib/cva.config'
+
+export const form = cx(
+	'grid gap-y-4 shadow',
+	'bg-stone-100',
+	'dark:bg-stone-600',
+	'shadow-stone-950/50 rounded-md p-4 max-w-96',
+)

61ba8f42490ace82b1c9368f1bbbee2cd7a8bc40
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-07 23:24:48 +0800

    Add missing form export

--- ui/form.tsx
+++ ui/form.tsx
@@ -13,6 +13,13 @@ import {
 import { cx } from "@/lib/cva.config"
 import { Label } from "@/ui/label"
 
+export const form = cx(
+  'grid gap-y-4 shadow',
+  'bg-stone-100',
+  'dark:bg-stone-600',
+  'shadow-stone-950/50 rounded-md p-4 max-w-96',
+)
+
 const Form = FormProvider
 
 type FormFieldContextValue<

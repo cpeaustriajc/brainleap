0bc48e238ac0f1774c5e1d6c5487c66887cac2e4
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-07 22:59:30 +0800

    Redesign signin page a bit

--- app/(auth)/auth/signin/page.tsx
+++ app/(auth)/auth/signin/page.tsx
@@ -1,47 +1,114 @@
+'use client'
+
 import { signInWithCredentials } from '@/lib/actions/auth'
-import { button } from '@/ui/button'
-import { form } from '@/ui/form'
-import { input } from '@/ui/input'
-import { label } from '@/ui/label'
+import { Button, buttonVariants } from '@/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/ui/card'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/ui/form'
+import { Input } from '@/ui/input'
 import { link } from '@/ui/link'
+import { zodResolver } from '@hookform/resolvers/zod'
 import Link from 'next/link'
+import { startTransition } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+	email: z.string().email(),
+	password: z.string()
+})
 
 export default function Page() {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: "",
+			password: ""
+		},
+		progressive: true
+	})
+
+	function onSubmit(e: z.infer<typeof formSchema>) {
+		// Temporary solution until https://github.com/react-hook-form/react-hook-form/pull/11061 gets merged.
+		const formData = new FormData();
+		formData.append('email', e.email);
+		formData.append('password', e.password);
+
+		startTransition(() => {
+			signInWithCredentials(formData)
+		});
+	}
+
 	return (
-		<form className={form}>
-			<header className="max-w-prose">
-				<h2 className="font-bold text-xl text-pretty">
+		<Card>
+			<CardHeader className='space-y-1'>
+				<CardTitle>
 					Sign-in to Brainleap 🧠
-				</h2>
-				<p>Sign-in with Google, Email, or Email and Password</p>
-			</header>
-			<label className={label}>Email</label>
-			<input className={input} type="email" name="email" id="email" />
-			<label className={label}>Password</label>
-			<input className={input} type="password" name="password" id="password" />
-			<button
-				formAction={signInWithCredentials}
-				className={button}
-				type="submit"
-			>
-				Sign In
-			</button>
-			<div className="flex gap-4">
-				<Link href="/auth/signin/google" className={link}>
-					Sign In With Google
-				</Link>
-				<Link href="/auth/signin/email" className={link}>
-					Sign In With Email
-				</Link>
-			</div>
-			<div className="flex justify-center">
+				</CardTitle>
+				<CardDescription>
+					Sign-in with Google, Email, or Email and Password
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<div className="flex gap-2 justify-between py-4">
+					<Link href="/auth/signin/google" className={buttonVariants({ variant: "outline" })}>
+						Sign In With Google
+					</Link>
+					<Link href="/auth/signin/email" className={buttonVariants({ variant: "outline" })}>
+						Sign In With Email
+					</Link>
+				</div>
+				<div className="relative">
+					<div className="absolute inset-0 flex items-center">
+						<span className="w-full border-t" />
+					</div>
+					<div className="relative flex justify-center text-xs uppercase">
+						<span className="bg-background px-2 text-muted-foreground">
+							Or continue with
+						</span>
+					</div>
+				</div>
+				<Form {...form}  >
+					<form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-8">
+						<FormField control={form.control} name="email" render={({ field }) => (
+							<FormItem>
+								<FormLabel>
+									Email
+								</FormLabel>
+								<FormControl>
+									<Input type="email" {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)} />
+						<FormField control={form.control} name="password" render={({ field }) => (
+							<FormItem>
+								<FormLabel >Password</FormLabel>
+								<FormControl>
+									<Input type="password" {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)} />
+						<Button
+							formAction={signInWithCredentials}
+							className="w-full cursor-default"
+							type="submit"
+						>
+							Sign In
+						</Button>
+					</form>
+				</Form>
+
+			</CardContent>
+			<CardFooter className='justify-center'>
 				<p>
 					Don't have an account?{' '}
 					<Link className={link} href="/auth/signup">
 						Sign Up
 					</Link>
 				</p>
-			</div>
-		</form>
+			</CardFooter>
+		</Card>
 	)
 }
+

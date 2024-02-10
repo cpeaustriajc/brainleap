34e143833255c2b5f58cdc97e98ffcd381922a76
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-10 18:16:31 +0800

    Remove statically generated params course layout

--- app/(dashboard)/course/[id]/layout.tsx
+++ app/(dashboard)/course/[id]/layout.tsx
@@ -1,36 +1,8 @@
-import { createClient } from '@/lib/supabase/static'
-import { Tab, TabItem, TabList, TabPanel, Tabs } from '@/ui/tabs'
+import { createServerClient } from '@supabase/ssr'
 import React from 'react'
 
-export async function generateStaticParams() {
-  const supabase = createClient()
-
-  const { data, error } = await supabase.from('courses').select('course_id')
-
-  if (error) throw error
-
-  return data.map(id => ({
-    params: {
-      id,
-    },
-  }))
-}
-
 export default function CourseLayout({
-  tabs,
-  params,
-}: { tabs: React.ReactNode; params: { id: string } }) {
-  return (
-    <React.Fragment>
-      <Tabs>
-        <TabList>
-          <Tab href={`/course/${params.id}`}>Announcements</Tab>
-          <Tab href={`/course/${params.id}/assignments`}>Assignments</Tab>
-          <Tab href={`/course/${params.id}/grades`}>Grades</Tab>
-          <Tab href={`/course/${params.id}/people`}>People</Tab>
-        </TabList>
-        <TabPanel>{tabs}</TabPanel>
-      </Tabs>
-    </React.Fragment>
-  )
+  children,
+}: { children: React.ReactNode; params: { id: string } }) {
+  return <React.Fragment>{children}</React.Fragment>
 }

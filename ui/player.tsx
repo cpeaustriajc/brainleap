3374b8d02ad9e6087b0f566788c193eb496a9d92
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-12 23:02:52 +0800

    Support adding thumbnail, src, title and timeline thumbnails

--- ui/player.tsx
+++ ui/player.tsx
@@ -16,7 +16,12 @@ import {
 
 import { VideoLayout } from './layouts/video-layout'
 
-export function Player() {
+export function Player({
+  src,
+  thumbnail,
+  thumbnails,
+  title,
+}: { src: string; thumbnail: string; thumbnails: string; title: string }) {
   let player = useRef<MediaPlayerInstance>(null)
 
   useEffect(() => {
@@ -49,7 +54,7 @@ export function Player() {
     <MediaPlayer
       className="w-full aspect-video bg-slate-900 text-white font-sans overflow-hidden rounded-md ring-media-focus data-[focus]:ring-4"
       title="Sprite Fight"
-      src="https://stream.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/low.mp4"
+      src={src}
       onProviderChange={onProviderChange}
       onCanPlay={onCanPlay}
       ref={player}
@@ -57,12 +62,12 @@ export function Player() {
       <MediaProvider>
         <Poster
           className="absolute inset-0 block h-full w-full rounded-md opacity-0 transition-opacity data-[visible]:opacity-100 object-cover"
-          src="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/thumbnail.webp?time=268&width=1200"
-          alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
+          src={thumbnail}
+          alt={title}
         />
       </MediaProvider>
 
-      <VideoLayout thumbnails="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/storyboard.vtt" />
+      <VideoLayout thumbnails={thumbnails} />
     </MediaPlayer>
   )
 }

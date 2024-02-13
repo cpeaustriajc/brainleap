9d3a3a953dbdccf8a878897bb95132c5aa1d51c8
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-13 08:25:42 +0800

    I remembered you can't actually use cache when using cookies lol

--- lib/queries/user.ts
+++ lib/queries/user.ts
@@ -1,25 +1,20 @@
 import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
-import { unstable_cache as cache } from 'next/cache'
 
-export const getUser = cache(
-  async () => {
-    const supabase = createClient()
-    const {
-      data: { user },
-      error,
-    } = await supabase.auth.getUser()
+export const getUser = async () => {
+  const supabase = createClient()
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser()
 
-    if (error) {
-      throw error
-    }
+  if (error) {
+    throw error
+  }
 
-    if (!user) {
-      redirect('/auth/signin')
-    }
+  if (!user) {
+    redirect('/auth/signin')
+  }
 
-    return user
-  },
-  ['user'],
-  { tags: ['user'] },
-)
+  return user
+}
